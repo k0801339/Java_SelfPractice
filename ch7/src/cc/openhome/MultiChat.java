f66957187400bf6e6ad4e1b988e3d7942e76a542
k0801339 <k0801339@gmail.com>
2017-03-06 23:34:54 +0800

    ch8

---
+++ ch7/src/cc/openhome/MultiChat.java
@@ -0,0 +1,25 @@
+package cc.openhome;
+
+public class MultiChat {
+	public static void main(String[] args){
+		Client c1 = new Client("127.0.0.1", "Caterpillar");
+		Client c2 = new Client("192.168.0.2", "Justin");
+		ClientQueue queue = new ClientQueue();
+		queue.addClientListener(new ClientListener() {
+			@Override
+			public void clientAdded(ClientEvent event){
+				System.out.printf("%s from %s connected\n", event.getName(), event.getIp());
+			}
+			@Override
+			public void clientRemoved(ClientEvent event){
+				System.out.printf("%s from %s disconnected\n", event.getName(), event.getIp());
+			}
+		});
+		queue.add(c1);
+		queue.add(c2);
+		
+		queue.remove(c1);
+		queue.remove(c2);
+	}
+	
+}
---
+++ ch8/src/cc/openhome/Average2.java
@@ -0,0 +1,10 @@
+package cc.openhome;
+
+public class Average2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
